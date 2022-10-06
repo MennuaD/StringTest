e57a211401e9caca29cde976639140be3d67e418
student <student@email.com>
2022-10-06 12:34:43 -0400

    Initial commit

---
+++ src/Creator.java
@@ -0,0 +1,5 @@
+public class Creator {
+    public class locationColon {
+
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,15 @@
+public class Runner {
+    public static void main(String[] args) {
+      /* String myString = "I am in 11th grade";
+       System.out.println(myString.substring(8,18));
+       myString = myString.substring(8,14);
+       System.out.println(myString);
+       System.out.println(myString.substring(8,18));*/
+
+        String message1 = "name:message";
+        String message2 = "hostname:message1341";
+        String message3 = "h:messagetesting";
+
+        System.out.println(message1.substring(0, locationColon));
+    }
+}
